42cb11fc9b4962e36b9ebc6bae652a7ca38f8d09
itsmanishsingh <itsmanishsingh6@gmail.com>
2023-01-28 19:59:59 +0530

    Updated the bug

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,11 @@ const express = require("express");
 const userRoutes = require('./routes/userRoutes')
 const connectToDB = require('./config/db')
 const app = express();
-const cors = require("cors");
+// const cors = require("cors");
 //Middleware
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
 
 // Connecting with DataBase
 connectToDB();
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,8 @@ import "./App.css";
 import Form from "./components/Form";
 import UserList from "./components/UserList";
 
-const BASE_URL = "https://crud-production-384a.up.railway.app";
-
+const BASE_URL = "https://crudapplication-production.up.railway.app";
+crudapplication-production.up.railway.app
 function App() {
   const [userData, setUserData] = useState(null);
   const fetchUsersData = async () => {
